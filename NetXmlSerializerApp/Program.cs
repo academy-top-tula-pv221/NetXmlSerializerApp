dab19045d51daa662dc10566490cb18a3edbf148
maxefimov <efimov.academy.top@gmail.com>
2023-03-17 21:07:01 +0300

    Добавьте файлы проекта.

---
+++ NetXmlSerializerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExampleClassLibrary;
+using System.Xml;
+using System.Xml.Serialization;
+
+List<User> users = new List<User>()
+{
+    new User("Joe", 34, new Address(){ City = "Moscow", Street = "Tverskaya"}),
+    new User("Tom", 41, new Address(){ City = "Orel", Street = "Lenina"}),
+};
+
+XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+
+//using (FileStream stream = new FileStream(@"D:\myusers2.xml", FileMode.Create))
+//{
+//    serializer.Serialize(stream, users);
+//}
+
+using (FileStream stream = new FileStream(@"D:\myusers2.xml", FileMode.Open))
+{
+    List<User> users2 = (List<User>)serializer.Deserialize(stream)!;
+    foreach(User user in users)
+        Console.WriteLine(user);
+}
